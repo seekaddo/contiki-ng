0ac71b0fd6cb4ed1aec26637f5991146cdf8d244
seekaddo <dennis_addo@aol.com>
2022-12-13 12:37:09 -0800

    first version of uIP with backend for quic- packetqueues needed

--- os/net/app-layer/quic-en/quic-uip.c
+++ os/net/app-layer/quic-en/quic-uip.c
@@ -45,6 +45,19 @@ static struct uip_udp_conn *udp_conn = NULL;
 static quic_udp_callback qreceive = NULL;
 //static uint8_t syncReq = 0;
 
+uint8_t syncNewData = 0;
+
+
+uint8_t quic_udp_active()
+{
+  return udp_conn != NULL;
+
+}
+
+struct uip_udp_conn *quic_udp_con()
+{
+  return udp_conn;
+}
 /*---------------------------------------------------------------------------*/
 void
 quic_endpoint_log(const quic_endpoint_t *ep)
@@ -119,6 +132,18 @@ get_src_endpoint(uint8_t secure)
   src.secure = 0;
   return &src;
 }
+
+void set_endpoint(quic_endpoint_t *ep)
+{
+  uip_ipaddr_copy(&ep->ipaddr, &UIP_IP_BUF->srcipaddr);
+  ep->port = UIP_UDP_BUF->srcport;
+  ep->secure = 0;
+}
+
+uint16_t get_ttl()
+{
+  return udp_conn->ttl;
+}
 /*---------------------------------------------------------------------------*/
 int
 quic_endpoint_is_secure(const quic_endpoint_t *ep)
@@ -130,6 +155,7 @@ int
 quic_endpoint_is_connected(const quic_endpoint_t *ep)
 {
 #ifndef CONTIKI_TARGET_NATIVE
+  //todo: remove this later from production code and use BLE version
   if(!uip_is_addr_linklocal(&ep->ipaddr)
     && NETSTACK_ROUTING.node_is_reachable() == 0) {
     return 0;
@@ -163,7 +189,7 @@ quic_endpoint_disconnect(quic_endpoint_t *ep)
 }
 /*---------------------------------------------------------------------------*/
 uint8_t *
-quic_databuf(void)
+quic_databuf(void) // call uip_datalen for the len
 {
   return uip_appdata;
 }
@@ -172,6 +198,8 @@ void
 quic_transport_init(void)
 {
   process_start(&quic_engine, NULL);
+  //todo: start another process to handle all quic request
+  // A pointer to the struct data is needed to pass the whole request
 
 }
 int quic_register(struct uip_udp_conn *udp_cn, const uint16_t port, quic_udp_callback callback)
@@ -187,15 +215,40 @@ int quic_register(struct uip_udp_conn *udp_cn, const uint16_t port, quic_udp_cal
   return 1;
 }
 
+uint32_t recvfr(uint8_t *buf, uint32_t len)
+{
+  if(syncNewData == 0)
+    return 0;
+
+  uint32_t akLen = uip_datalen();
+  if(uip_datalen() > len)
+  {
+    LOG_WARN("------Not Enough buffer provided by backend");
+    akLen = uip_datalen(); //
+  }
+
+  memcpy(buf,quic_databuf(),akLen);
+  syncNewData = 0;
+  return akLen;
+}
+
 /*---------------------------------------------------------------------------*/
 static void
 process_data(void)
 {
 
+  //todo:
+  //set active newdata on
+  syncNewData = 1;
+
   LOG_INFO("receiving UDP datagram from [");
   LOG_INFO_6ADDR(&UIP_IP_BUF->srcipaddr);
   LOG_INFO_("]:%u\n", uip_ntohs(UIP_UDP_BUF->srcport));
   LOG_INFO("  Length: %u ttl: %d \n", uip_datalen(), udp_conn->ttl);
+
+  char buf[UIPLIB_IPV6_MAX_STR_LEN];
+  uiplib_ipaddr_snprint(buf, sizeof(buf), &UIP_IP_BUF->srcipaddr);
+  LOG_INFO("  test uIP addr: %s \n", buf);
 
   if(qreceive != NULL)
   {
@@ -206,6 +259,12 @@ process_data(void)
   }
 
 }
+const char * wi_ntop(uip_ipaddr_t *add,  char * dest)
+{
+  uiplib_ipaddr_snprint(dest, 40, add);
+  return dest;
+}
+
 /*---------------------------------------------------------------------------*/
 int
 quic_sendto(const quic_endpoint_t *ep, const uint8_t *data, uint16_t length)
@@ -238,6 +297,9 @@ PROCESS_THREAD(quic_engine, ev, data)
   /* new connection with remote host */
   udp_conn = udp_new(NULL, 0, NULL);
   udp_bind(udp_conn, SERVER_LISTEN_PORT);
+  char buf[UIPLIB_IPV6_MAX_STR_LEN];
+  uiplib_ipaddr_snprint(buf, sizeof(buf), &uip_hostaddr);
+  LOG_INFO("  Client uIP addr: %s \n", buf);
   LOG_INFO("QUIC Listening on port %u\n", uip_ntohs(udp_conn->lport));
   LOG_INFO("QUIC ipv6 MTU: %d ttl:%d \n", UIP_LINK_MTU, udp_conn->ttl);
 
@@ -247,8 +309,13 @@ PROCESS_THREAD(quic_engine, ev, data)
 
     if(ev == tcpip_event) {
       if(uip_newdata()) {
-        process_data();
+        process_data(); // tood:DEE run loop_run here
       }
+
+      //todo: DEE loop_run state-machine (new)
+      //todo: create a separate thread for processing all QUIC request.
+      // No AutoStart Process but started on active connection
+      // Using the etimer in the thread
     }
   } /* while (1) */
 
